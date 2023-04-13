f1657cd747cb0b373db9e5781f7b7f0f03eba9a8
Alexander Korobov <dktwho@mail.ru>
2023-04-13 12:59:23 +0300

    modified import/export to one string reexporting

--- client/react-pizza/src/components/index.js
+++ client/react-pizza/src/components/index.js
@@ -1,9 +1,14 @@
-import Header from "./Header";
-import Categories from './Categories'
-import Button from "./Button";
+// import Header from "./Header";
+// import Categories from './Categories'
+// import Button from "./Button";
 
-export { 
-  Header, 
-  Categories,
-  Button
- }
+
+export {default as Button} from './Button'
+export {default as Categories} from './Categories'
+export {default as Header} from './Header'
+
+// export { 
+//   Header, 
+//   Categories,
+//   Button
+//  }
