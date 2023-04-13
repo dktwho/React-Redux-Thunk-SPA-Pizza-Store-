87ebdbd776d5a42f3b31f1f6e5187231a896f6a6
Alexander Korobov <dktwho@mail.ru>
2023-04-13 12:56:42 +0300

    create index.js with common imports components

--- client/react-pizza/src/App.js
+++ client/react-pizza/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Categories from "./components/Categories";
 
-import Header from "./components/Header";
+import { Header, Categories } from './components'
 
 
 
---
+++ client/react-pizza/src/components/index.js
@@ -0,0 +1,9 @@
+import Header from "./Header";
+import Categories from './Categories'
+import Button from "./Button";
+
+export { 
+  Header, 
+  Categories,
+  Button
+ }
