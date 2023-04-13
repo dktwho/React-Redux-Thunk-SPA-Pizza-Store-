cb1747f66b793680a6d280eea85effc1b9d7caae
Alexander Korobov <dktwho@mail.ru>
2023-04-13 12:50:06 +0300

    modified component button to header and create component Categories

--- client/react-pizza/src/App.js
+++ client/react-pizza/src/App.js
@@ -1,29 +1,20 @@
 import React from "react";
+import Categories from "./components/Categories";
 
-import Header from "./Header";
-import Button from "./Button";
+import Header from "./components/Header";
+
 
 
 function App() {
+
+  
   return (
     <div className="wrapper">
       <Header/>
-      <Button outline>Btn 1</Button>
-      <Button>Button 2</Button>
- 
     <div className="content">
       <div className="container">
         <div className="content__top">
-          <div className="categories">
-            <ul>
-              <li className="active">Все</li>
-              <li>Мясные</li>
-              <li>Вегетарианская</li>
-              <li>Гриль</li>
-              <li>Острые</li>
-              <li>Закрытые</li>
-            </ul>
-          </div>
+          <Categories/>
           <div className="sort">
             <div className="sort__label">
               <svg
--- client/react-pizza/src/components/Button.jsx
+++ client/react-pizza/src/components/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Button = ({ outline, children }) => {
+const Button = ({ outline, children, onClick, className }) => {
   return (
     <button
-      className={classNames('button', {
+      onClick={onClick}
+      className={classNames('button', className, {
         'button--outline': outline,
       })}
     >
