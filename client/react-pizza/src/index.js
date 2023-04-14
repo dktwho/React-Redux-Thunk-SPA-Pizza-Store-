6e0cbcde00619bac039a27a63ef891b1e54c4cdf
Alexander Korobov <dktwho@mail.ru>
2023-04-14 12:01:40 +0300

    modified arrow in sortPopus scss, add react router dom v6, create pages home, Cart, move landing in home, cart is default h1, add Link to navigate, create reexport for pages Home, Cart

--- client/react-pizza/src/index.js
+++ client/react-pizza/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import {BrowserRouter as Router} from 'react-router-dom'
 import './scss/app.scss';
 
 import App from './App';
@@ -8,9 +8,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <Router>
     <App />
-  // </React.StrictMode>
+  </Router>
+
 );
+
 
 
