5248a4e022510610cd3ff9b5db986fe2df51fd13
Alexander Korobov <dktwho@mail.ru>
2023-04-18 14:20:26 +0300

    add redux, react-redux

--- client/react-pizza/src/index.js
+++ client/react-pizza/src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom'
 import './scss/app.scss';
+import { Provider } from 'react-redux'
+import store from './redux/store';
 
 import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
+    <Provider store={store}>
     <App />
+    </Provider>
   </Router>
 
 );
