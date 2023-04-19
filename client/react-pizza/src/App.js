92a3ce74d0efdf8abf7f2ce3f08af75b954178ef
Alexander Korobov <dktwho@mail.ru>
2023-04-19 11:32:46 +0300

    perform App class to functional component

--- client/react-pizza/src/App.js
+++ client/react-pizza/src/App.js
@@ -7,37 +7,24 @@ import axios from 'axios'
 import setPizzas from "./redux/action/pizzas";
 import { connect } from 'react-redux'
 
-
-// function App() {
-//   const [pizzas, setPizzas] = useState([])
-
-//   useEffect(() => {
-//     axios.get('http://localhost:3000/db.json')
-//     .then(({ data }) => setPizzas(data.pizzas))
-    
-//   }, [])
- 
-//   return ;
-// }
-
-class App extends React.Component {
-  componentDidMount() {
+function App ({ setPizzas, items }) {
+  useEffect(() => {
     axios.get('http://localhost:3000/db.json')
-    .then(({ data }) => {this.props.dispatch(setPizzas(data.pizzas))})    
-  }
-  render() {
-    return (
-        <div className="wrapper">
-          <Header/>
-        <div className="content">
-        <Routes>
-            <Route path="/" element={<Home items={this.props.items} />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </div>
+    .then(({ data }) => {setPizzas(data.pizzas)})   
+  }, [])
+
+  return (
+    <div className="wrapper">
+      <Header/>
+      <div className="content">
+      <Routes>
+        <Route path="/" element={<Home items={items} />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
       </div>
-      );
-  }
+  </div>
+
+  )
 }
 
 const mapStateToProps = (state) => {
